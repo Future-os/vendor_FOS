38d8032d9dc0af9e268aef5c80311e0e5e287a78
Sankar <sankarapspk@users.noreply.github.com>
2019-01-18 05:22:56 -0800

    add future os bootanimation and branding

--- config/common.mk
+++ config/common.mk
@@ -178,3 +178,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/future/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/future/overlay/common
+
+# Future-OS Branding
+include vendor/future/config/branding.mk
+# Bootanimation
+include vendor/future/config/bootanimation.mk
